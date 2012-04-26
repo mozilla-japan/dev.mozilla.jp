7858e2c0fca1743612ecb60ae1554b386639f3f2
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-04-26 16:14:27 +0900

    Remove inline style from header.php(#40)

--- header.php
+++ header.php
@@ -19,19 +19,6 @@
       type="text/css" media="screen" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <style type="text/css" media="screen">
-  <?php // Checks to see whether it needs a sidebar or not
-        if( empty ($withcomments)  && !is_single ()  ) { ?> #page {
-    background:
-      url("<?php bloginfo('stylesheet_directory'); ?>/images/kubrickbg-<?php bloginfo('text_direction'); ?>.jpg")
-      repeat-y top;
-    border: none;
-  }
-  <?php } else { // No sidebar ?> #page { background:url("<?php bloginfo('stylesheet_directory'); ?>/images/kubrickbgwide.jpg")repeat-ytop;
-    border: none;
-  }
-  <?php
-     }
-     ?>
 </style>
   <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?> 
   <?php wp_enqueue_script('jquery'); ?>
