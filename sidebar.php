9ee0a16c7101a22a8d6740a4136167e9b8d34971
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-04-27 12:36:44 +0900

    サイドバーに表示する項目を修正 (#9)

--- sidebar.php
+++ sidebar.php
@@ -6,32 +6,35 @@
 ?>
 
 <nav id="sidebar">
-  <h3><a href="<?php bloginfo('url'); ?>/aboutmodest/">About</a></h3>
+
+  <h3><a href="">modestに参加する</a></h3>
+
+  <h3><a href="">イベントを追加する</a></h3>
+
+  <h3><a href="">ブログを投稿する</a></h3>
+
+  <h3><a href="<?php bloginfo('url'); ?>/aboutmodest/">About modest</a></h3>
 
   <h3>Search</h3>
   <?php get_search_form(); ?>
 
   <h3>
     <?php
-      // Output the link of "event" page.
-      $baseURL = get_bloginfo('url');
-      $catgoryPath = '/?post_type=event';
-      echo '<a href="' .  $baseURL . $catgoryPath . '">Event</a>';
-    ?>
-  </h3>
-
-  <h3>
-    <?php
       // Output the link of "project" page.
       $category_id = get_cat_ID('projects');
       $category_link = get_category_link($category_id);
-      echo '<a href="' . $category_link . '">Project</a>';
+      echo '<a href="' . $category_link . '">プロジェクト</a>';
     ?>
   </h3>
   <ul>
     <?php wp_list_categories('title_li='); ?>
   </ul>
 
+  <?php 
+    /**
+     * This login/out link is for debug
+     */
+  ?>
   <aside>
     <ul>
       <li><?php wp_register(); ?></li>
