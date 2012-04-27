e6b66c39b50c4b6db63b02e2cf7bfc01b0274b49
https://bitbucket.org/EnsekiTT <devnull@localhost>
2012-04-27 17:14:30 +0900

    イベントボックスの作成 (#13)

--- index.php
+++ index.php
@@ -31,6 +31,30 @@ get_header();
 
   <section id="event">
     <h2>イベント</h2>
+    <div id="event-contents">
+      <?php 
+         $posts = get_posts( array( 'post_type' => 'event' ) );
+         $postcount = 0;
+         ?>
+      <?php if($posts): foreach($posts as $post): setup_postdata($post); ?>
+      <?php if($postcount == 0){ ?>
+      <dev id="event-left">
+        <?php the_time('y-m-d (D)');?> :
+        <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
+          <?php the_title(); ?>
+        </a>
+        <p> <?php the_content(); ?> </p>
+      </dev>
+      <?php } else { ?>
+      <dev id="event-right">
+        <?php the_time('y-m-d (D)');?> :
+        <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
+          <?php the_title(); ?>
+        </a>
+      </dev>
+      <?php } ?>
+      <?php $postcount++; endforeach; endif;?>
+    </div>
   </section>
 
   <section id="blogfeed">
