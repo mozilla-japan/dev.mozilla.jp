e5e4d7c27940cf49f37ddf39c9460346375bd810
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-04-27 15:31:50 +0900

    グローバルなheader要素にid=headerを設定する (#54)

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<header>
+<header id="header">
   <h1><a href = "<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
 </header>
 <div id="page">
