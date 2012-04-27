4814c54af2499be4f896e49f06213ee3afeaf3a6
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-04-27 14:25:30 +0900

    Join Us 用のリンクにスタイルを付ける(#9)

--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,11 @@
 
 <nav id="sidebar">
 
-  <h3><a href="">modestに参加する</a></h3>
+  <h3><a href="" class="button">modestに参加する</a></h3>
 
-  <h3><a href="">イベントを追加する</a></h3>
+  <h3><a href="" class="button">イベントを追加する</a></h3>
 
-  <h3><a href="">ブログを投稿する</a></h3>
+  <h3><a href="" class="button">ブログを投稿する</a></h3>
 
   <h3><a href="<?php bloginfo('url'); ?>/aboutmodest/">About modest</a></h3>
 
