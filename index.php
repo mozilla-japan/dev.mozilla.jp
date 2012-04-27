5e2e1277aba7aa5e6b65b2d3a4040b1662f457d0
https://bitbucket.org/EnsekiTT <devnull@localhost>
2012-04-27 16:57:44 +0900

    注目トピックを表示できるようにする。(#12)

--- index.php
+++ index.php
@@ -8,9 +8,25 @@ Template Name: index
 get_header();
 ?>
 <article id="content">
-
   <section id="hottopic">
     <h2>注目トピック</h2>
+    <div id="hottopic-contents">
+      <?php
+         $cat = get_query_var('hot');
+         $posts = get_posts("order=desc&category=$cat&numberposts=6");
+         ?>
+      <ul> 
+        <?php
+           if($posts): foreach($posts as $post): setup_postdata($post); ?>
+        <li id="catpost-<?php the_ID(); ?>">
+          <?php the_time('y-m-d (D)'); ?> :
+          <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
+            <?php the_title(); ?>
+          </a>
+        </li>
+        <?php endforeach; endif;?>
+      </ul>
+    </div>
   </section>
 
   <section id="event">
