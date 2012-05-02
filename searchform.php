81c10299ff13a348496ea6943658751d46ded9ea
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-05-02 23:21:52 +0900

    searchform.phpの追加 (#74)

---
+++ searchform.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage modest3
+ */
+?>
+
+<form id="searchform"
+      class=""
+      role="search"
+      method="get"
+      action="<?php echo home_url( '/' ); ?>">
+  <label for="search_field">åüçı :</label>
+  <input id="s"
+         class=""
+         type="text"
+         name="s"
+         value="<?php the_search_query(); ?>" />
+  <input id="searchsubmit"
+         class="button-white"
+         type="submit"
+         value="åüçı" />
+</form>
