65142032edb10a07310f90f775e47c7076bf7b0f
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-05-07 17:23:28 +0900

    フッターを#pageの子孫要素から外す(#82)

--- footer.php
+++ footer.php
@@ -8,100 +8,101 @@
 
 		<?php get_sidebar(); ?>
 
-		<footer id="footer">
-			<!-- Mozilla 関連サイト -->
-			<section id="mozilla_ring">
-
-				<h3 id="mozilla_ring_title">Mozilla 関連サイト</h3>
-
-				<div class="mozilla_ring_category">
-					<h5 class="mozilla_ring_category_title">ニュース</h5>
-					<ul>
-						<li class="mozilla_ring_menu">
-							<a href="http://mozillazine.jp/">MozillaZine.jp</a>
-						</li>
-						<li class="mozilla_ring_menu">
-							<a href="http://mozlinks-jp.blogspot.com/">Mozilla Links 日本語版</a>
-						</li>
-					</ul>
-				</div>
-
-				<div class="mozilla_ring_category">
-					<h5 class="mozilla_ring_category_title">コミュニティ</h5>
-					<ul>
-						<li class="mozilla_ring_menu">
-							<a href="http://mozillazine.jp/">MozillaZine.jp</a>
-						</li>
-						<li class="mozilla_ring_menu">
-							<a href="http://www.mozilla.gr.jp/">もじら組</a>
-						</li>
-						<li class="mozilla_ring_menu">
-							<a href="http://jpmoz.net/">mcnj</a>
-						</li> <!-- ul class="mozilla_ring_menu_current"><span>modest</span></ul -->
-					</ul>
-				</div>
-
-				<div class="mozilla_ring_category">
-					<h5 class="mozilla_ring_category_title">開発</h5>
-					<ul>
-						<li class="mozilla_ring_menu">
-							<a href="http://bugzilla.mozilla.gr.jp/">Bugzilla-jp</a>
-						</li>
-						<li class="mozilla_ring_menu">
-							<a href="https://developer.mozilla.org/ja">MDC</a>
-						</li>
-						<li class="mozilla_ring_menu">
-							<a href="http://forums.firehacks.org/l10n/">l10n forum</a>
-						</li> <!-- ul class="mozilla_ring_menu_current"><span>modest</span></ul -->
-					</ul>
-				</div>
-
-				<div class="mozilla_ring_category">
-					<h5 class="mozilla_ring_category_title">ドキュメント</h5>
-					<ul>
-						<li class="mozilla_ring_menu">
-							<a href="https://developer.mozilla.org/ja">MDC</a>
-						</li>
-						<li class="mozilla_ring_menu">
-							<a href="http://support.mozilla.com/ja/">SUMO</a>
-						</li>
-						<li class="mozilla_ring_menu">
-							<a href="http://forums.firehacks.org/trans/">翻訳 forum</a>
-						</li>
-					</ul>
-				</div>
-
-				<div class="mozilla_ring_category">
-					<h5 class="mozilla_ring_category_title">オフィシャル</h5>
-					<ul>
-						<li class="mozilla_ring_menu">
-							<a href="http://mozilla.jp/">Mozilla Japan</a>
-						</li>
-						<li class="mozilla_ring_menu">
-							<a href="http://www.mozilla.com/">Mozilla.com</a>
-						</li>
-						<li class="mozilla_ring_menu">
-							<a href="http://www.mozilla.org/">Mozilla.org</a>
-						</li>
-						<li class="mozilla_ring_menu">
-							<a href="http://mozillalabs.com/">Mozilla Labs</a>
-						</li>
-					</ul>
-				</div>
-
-			</section>
-
-			<section>
-				<p id="copyright">
-					Portions of this content are ©2009-2012 by individual dev.mozilla.jp
-					contributors.<br /> Content available under a Creative Commons <a href="/about/license/">license</a>.
-				</p>
-			</section>
-
-		<!-- #footer -->
-		</footer>
 	<!-- #container -->
 	</div>
+
+	<footer id="footer">
+		<!-- Mozilla 関連サイト -->
+		<section id="mozilla_ring">
+
+			<h3 id="mozilla_ring_title">Mozilla 関連サイト</h3>
+
+			<div class="mozilla_ring_category">
+				<h5 class="mozilla_ring_category_title">ニュース</h5>
+				<ul>
+					<li class="mozilla_ring_menu">
+						<a href="http://mozillazine.jp/">MozillaZine.jp</a>
+					</li>
+					<li class="mozilla_ring_menu">
+						<a href="http://mozlinks-jp.blogspot.com/">Mozilla Links 日本語版</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="mozilla_ring_category">
+				<h5 class="mozilla_ring_category_title">コミュニティ</h5>
+				<ul>
+					<li class="mozilla_ring_menu">
+						<a href="http://mozillazine.jp/">MozillaZine.jp</a>
+					</li>
+					<li class="mozilla_ring_menu">
+						<a href="http://www.mozilla.gr.jp/">もじら組</a>
+					</li>
+					<li class="mozilla_ring_menu">
+						<a href="http://jpmoz.net/">mcnj</a>
+					</li> <!-- ul class="mozilla_ring_menu_current"><span>modest</span></ul -->
+				</ul>
+			</div>
+
+			<div class="mozilla_ring_category">
+				<h5 class="mozilla_ring_category_title">開発</h5>
+				<ul>
+					<li class="mozilla_ring_menu">
+						<a href="http://bugzilla.mozilla.gr.jp/">Bugzilla-jp</a>
+					</li>
+					<li class="mozilla_ring_menu">
+						<a href="https://developer.mozilla.org/ja">MDC</a>
+					</li>
+					<li class="mozilla_ring_menu">
+						<a href="http://forums.firehacks.org/l10n/">l10n forum</a>
+					</li> <!-- ul class="mozilla_ring_menu_current"><span>modest</span></ul -->
+				</ul>
+			</div>
+
+			<div class="mozilla_ring_category">
+				<h5 class="mozilla_ring_category_title">ドキュメント</h5>
+				<ul>
+					<li class="mozilla_ring_menu">
+						<a href="https://developer.mozilla.org/ja">MDC</a>
+					</li>
+					<li class="mozilla_ring_menu">
+						<a href="http://support.mozilla.com/ja/">SUMO</a>
+					</li>
+					<li class="mozilla_ring_menu">
+						<a href="http://forums.firehacks.org/trans/">翻訳 forum</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="mozilla_ring_category">
+				<h5 class="mozilla_ring_category_title">オフィシャル</h5>
+				<ul>
+					<li class="mozilla_ring_menu">
+						<a href="http://mozilla.jp/">Mozilla Japan</a>
+					</li>
+					<li class="mozilla_ring_menu">
+						<a href="http://www.mozilla.com/">Mozilla.com</a>
+					</li>
+					<li class="mozilla_ring_menu">
+						<a href="http://www.mozilla.org/">Mozilla.org</a>
+					</li>
+					<li class="mozilla_ring_menu">
+						<a href="http://mozillalabs.com/">Mozilla Labs</a>
+					</li>
+				</ul>
+			</div>
+
+		</section>
+
+		<section>
+			<p id="copyright">
+				Portions of this content are ©2009-2012 by individual dev.mozilla.jp
+				contributors.<br /> Content available under a Creative Commons <a href="/about/license/">license</a>.
+			</p>
+		</section>
+
+	</footer>
+
 <!-- page_menu -->
 
 		<?php wp_footer(); ?>
