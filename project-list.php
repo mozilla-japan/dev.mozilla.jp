181921c16c4c16fe28755118ac1806b86b877906
https://bitbucket.org/EnsekiTT <devnull@localhost>
2012-05-07 15:30:16 +0900

    プロジェクトリストの作成(#79)

--- project-list.php
+++ project-list.php
@@ -15,28 +15,45 @@ get_header();
       </a>
     </h2>
   </hgroup>
-
-  <?php
-    $category_ids = get_all_category_ids();
-    foreach ($category_ids as $cat_id) :
-      $catObj = get_category($cat_id, 'OBJECT', 'raw');
-      $catLink = get_category_link($cat_id);
-  ?>
+      <?php
+         $paged = get_query_var('paged');
+         $posts = query_posts(array("post_type" => array("project"),
+                                    "paged" => $paged,
+                                    "posts_par_page" => 5,
+                                    "meta_key " => $_GET['key'],
+                                    "meta_value" => $_GET['value'],
+                                    "orderby" => "date"));
+      ?>
+      <nav class="navigation">
+        <?php wp_pagenavi(); ?>
+      </nav>
+      <?php
+        if(have_posts()) :
+          foreach ($posts as $post) :
+            setup_postdata($post);
+      ?>
     <section class="project_summary">
       <?php
-        //categoryごとのimg要素を設定する
-        //$imgSrc = get_bloginfo("template_url") . '/images/icons/modest_projects.png';
-      ?>
+         //categoryごとのimg要素を設定する
+         //$imgSrc = get_bloginfo("template_url") . '/images/icons/modest_projects.png';
+         ?>
       <img class="project_icon" src=""/>
       <h1 class="project_title">
-        <a href="<?php echo($catLink) ?>"><?php echo($catObj->name) ?></a>
+        <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
+          <?php the_title(); ?>
+        </a>
       </h1>
-      <p class="project_description"><?php echo($catObj->description) ?></p>
+      <p class="project_description">
+        <?php the_excerpt(); ?>
+      </p>
     </section>
-  <?php
-    endforeach;
-  ?>
-
+    <?php
+       endforeach;
+       endif;
+    ?>
+    <nav class="navigation">
+      <?php wp_pagenavi(); ?>
+    </nav>
 </article>
 
 <?php get_footer(); ?>
