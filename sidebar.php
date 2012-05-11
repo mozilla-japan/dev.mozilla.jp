883d4db2ca51991bbf3bab1ccf50f902c5f7cdd5
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-05-11 23:48:44 +0900

    サイドバーのボタンをまとめるdivを追加

--- sidebar.php
+++ sidebar.php
@@ -7,20 +7,24 @@
 
 <nav id="sidebar">
 
-<?php if(is_user_logged_in() == false) : ?>
-  <h3><a href="<?php echo get_join_modest_url(); ?>" class="button-blue">ログインする/参加する</a></h3>
-<?php endif; ?>
+  <div class="button-group">
 
-  <h3><a href="<?php echo get_add_post_url(); ?>" class="button-blue">記事を投稿する</a></h3>
+    <?php if(is_user_logged_in() == false) : ?>
+      <h3><a href="<?php echo get_join_modest_url(); ?>" class="button-blue">ログインする/参加する</a></h3>
+    <?php endif; ?>
 
-  <h3><a href="<?php echo get_add_event_url(); ?>" class="button-blue">イベントを追加する</a></h3>
+    <h3><a href="<?php echo get_add_post_url(); ?>" class="button-blue">記事を投稿する</a></h3>
 
-  <?php if( is_user_logged_in() ) : ?>
-  <h3><a href="<?php echo get_add_project_url(); ?>" class="button-blue">プロジェクトを作成</a></h3>
+    <h3><a href="<?php echo get_add_event_url(); ?>" class="button-blue">イベントを追加する</a></h3>
 
-  <h3><?php wp_loginout(); ?></h3>
+    <?php if( is_user_logged_in() ) : ?>
+      <h3><a href="<?php echo get_add_project_url(); ?>" class="button-blue">プロジェクトを作成</a></h3>
 
-  <?php endif; ?>
+      <h3><?php wp_loginout(); ?></h3>
+
+    <?php endif; ?>
+
+  </div>
 
   <h3 id="about"><a href="<?php echo get_about_url(); ?>">このサイトについて</a></h3>
 
