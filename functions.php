cf5f03ee2c374ecd2f16d5968d8f955248ace920
EnsekiTT <devnull@localhost>
2012-05-11 16:36:13 +0900

    get_the_specified_project_pageを実装　引数はカテゴリID

--- functions.php
+++ functions.php
@@ -816,9 +816,20 @@ function get_add_post_url(){
   $path = '/addpost/';
   return ($base . $path);
 }
+
 /* return the url of a specified project page (string) */
-function get_the_specified_project_page () {
-  return '';
+function get_the_specified_project_page ($cat_id) {
+  $page_id = get_project_page_ID($cat_id);
+  return (get_permalink($page_id));
+}
+/* return the ID of a specified project page */
+function get_project_page_ID ($cat_id) {
+  if(isset($cat_id)){
+    $t_id = $cat_id;
+    $cat_meta = get_option( "cat_$t_id" );
+    $project_id = $cat_meta['project_id'];
+    return $project_id;
+  }
 }
 
 
