38cadf0cd7a3e9909728de66cac7cab5c28dda01
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-05-14 16:16:26 +0900

    ブログフィード部分のマークアップの修正: @part2(#75)

--- index.php
+++ index.php
@@ -92,6 +92,7 @@ DOC;
       if($posts):
         foreach($posts as $post):
           setup_postdata($post);
+          $id = get_the_ID();
     ?>
     <article class="blogfeed-article">
       <header>
@@ -109,7 +110,7 @@ DOC;
         ?>
         <ul>
             <?php
-                $catlist = get_the_category();
+                $catlist = get_the_category($id);
                 foreach ($catlist as $cat) :
                     $project_array = get_post(get_project_page_ID($cat->cat_ID));
                     $link = get_permalink($project_array->ID);
