80938a8c75b20bc360b3641253edb6752a848e88
EnsekiTT <devnull@localhost>
2012-05-17 16:25:07 +0900

    search結果をアーカイブと同じ表示にする(#110)

--- search.php
+++ search.php
@@ -11,29 +11,95 @@ get_header(); ?>
   <?php if (have_posts()) : ?>
   <h2 class="pagetitle">
 	<?php _e('Search Results', 'kubrick'); ?></h2>
-  <div class="navigation">
-	<div class="alignleft">
-	  <?php previous_posts_link(__('&laquo; 新しい投稿へ', 'kubrick')) ?></div>
-	<div class="alignright">
-	  <?php next_posts_link(__('前の投稿へ &raquo;', 'kubrick')) ?></div>
-  </div>
+  <nav class="navigation">
+    <?php wp_pagenavi(); ?>
+  </nav>
   <?php while (have_posts()) : the_post(); ?>
-  <div <?php post_class();
-             $flag = odd_even($flag);
-             ?>>
+  	<article class="archive-post">
 
-    <?php
-       post_icon(get_the_category()); ?>
-	<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'kubrick'), the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></h3>
-	<small><?php the_time('l, F jS, Y') ?></small>
-    <small><?php the_author_posts_link() ?></small>
-	<p class="postmetadata"><?php the_tags(__('Tags:', 'kubrick') . ' ', ', ', '<br />'); ?> <?php printf(__('Posted in %s', 'kubrick'), get_the_category_list(', ')); ?> | <?php edit_post_link(__('Edit', 'kubrick'), '', ' | '); ?>  <?php comments_popup_link(__('No Comments &#187;', 'kubrick'), __('1 Comment &#187;', 'kubrick'), __('% Comments &#187;', 'kubrick'), '', __('Comments Closed', 'kubrick') ); ?></p>
-  </div>
+		<header class="archive-post_header">
+			<?php
+				post_icon(get_the_ID(),array(70,70));
+			?>
+
+			<?php
+				/*
+				* article's title
+				*/
+				$permaLink = get_permalink();
+				$titleText = get_the_title();
+				echo '<h1 class="archive-post-title"><a href="'. $permaLink .'">'. $titleText .'</a></h1>';
+			?>
+
+			<?php
+				if (is_user_logged_in()) :
+					$edit_link = get_edit_post_link($the_id);
+					//here document
+					echo <<< DOC
+					<a href="$edit_link"
+					   class="edit_post button-white">編集する</a>
+DOC;
+				endif;
+			?>
+		</header>
+
+		<footer class="meta-container">
+
+			<div class="postmeta-project">
+				<ul class="meta-project-list">
+					<?php
+						$catlist = get_the_category();
+						foreach ($catlist as $cat) :
+							$project_array = get_post(get_project_page_ID($cat->cat_ID));
+							if ($project_array->post_type == 'project') :
+								$link = get_permalink($project_array->ID);
+								$link_text = $project_array->post_title;
+								//here doc:
+								echo <<< DOC
+								<li>
+									<a href="$link">$link_text</a>
+								</li>
+DOC;
+							endif;
+						endforeach;
+					?>
+				</ul>
+			</div>
+
+		</footer>
+
+		<div class="entry-body">
+			<?php the_content() ?>
+		</div>
+
+		<footer class="entry-footer">
+			<div class="postmeta">
+				<p class="postmeta-title">投稿者</p>
+				<address class="postmeta-content">
+					<?php
+						$post = get_post($the_id);
+						$userID = $post->post_author;
+						echo get_avatar($userID, 15);//avatar image
+						echo the_author_posts_link();//auther link
+					?>
+				</address>
+				<p class="postmeta-title">投稿日時</p>
+				<div class="postmeta-content">
+					<?php
+						$datetime = get_the_time('Y-m-d H:i:s');
+						$date = get_the_time('Y年n月j日 G:i:s');
+						echo('<time datetime="' . $datetime . '">'. $date . '</time>');
+					?>
+				</div>
+			</div>
+		</footer>
+
+	</article>
+
   <?php endwhile; ?>
-  <div class="navigation">
-	<div class="alignleft"><?php previous_posts_link(__('&laquo; 新しい投稿へ', 'kubrick')) ?></div>
-	<div class="alignright"><?php next_posts_link(__('前の投稿へ &raquo;', 'kubrick')) ?></div>
-  </div>
+  <nav class="navigation">
+    <?php wp_pagenavi(); ?>
+  </nav>
   <?php else : ?>
   <h2 class="center">残念ながら、キーワードに一致する記事は見つけられませんでした。<br>別のキーワードをお試しください。</h2>
   <?php get_search_form(); ?>
