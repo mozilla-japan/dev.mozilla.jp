cf0931a63b4751d18cfdf2953451081dcede56e7
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-05-18 18:33:07 +0900

    single-project.phpのマークアップを整理(#99)

--- single-project.php
+++ single-project.php
@@ -9,6 +9,7 @@ get_header();
 
 <article id="content"
          role="main">
+
 	<?php
 		if (have_posts()) :
 			while (have_posts()) :
@@ -19,7 +20,6 @@ get_header();
 	?>
 
 	<header class="entry-header">
-
 		<?php
 			//post_icon($the_id,array(120,120));
 		?>
@@ -29,20 +29,7 @@ get_header();
 		<?php
 			edit_the_link($the_id);
 		?>
-    <?php
-       /*
-       button-blueとかにすれば分かりやすいかも？
-       */
-       $url = get_post_meta($post->ID, 'url', true);
-       echo "<a href=". $url .">プロジェクトのWebサイト</a>";
-    ?>
 	</header>
-
-	<div class="entry-body">
-		<?php
-			the_content();
-		?>
-	</div>
 
 	<footer class="entry-footer">
 		<div class="postmeta">
@@ -55,84 +42,90 @@ get_header();
 					echo the_author_posts_link();//auther link
 				?>
 			</address>
-		</div>    
+
+			<p class="postmeta-title">Web サイト</p>
+			<address class="postmeta-content">
+				<?php
+					$url = get_post_meta($post->ID, 'url', true);
+					if ($url) :
+						echo '<a href="'. $url .'">プロジェクトのWebサイト</a>';
+					else :
+						echo 'なし';
+					endif;
+				?>
+			</address>
+		</div>
 	</footer>
 
-  <?php
-	  	endwhile;
-	  else:
-		  echo '<p>Sorry, no posts matched your criteria</p>';
-	  endif;
-  ?>
+	<div class="entry-body">
+		<?php
+			the_content();
+		?>
+	</div>
 
-<h1>プロジェクトの最新の投稿</h1> 
-  <section>
-    <?php $cat_id = get_post_meta($post->ID, 'cat', true); 
-    $args = array('posts_per_page' => 5,
-                  'category' => $cat_id);
-    $posts = get_posts($args);
-    if($posts):
-      foreach($posts as $post):
-        setup_postdata($post);
-        $the_id = get_the_ID();
-    ?>
-	  <article class="archive-post">
-		  <header class="archive-post_header">
-			  <?php
-				   //post_icon(get_the_ID(),array(70,70));
-			     ?>
-        
-			  <?php
-				   /*
-				   * articles title
-				   */
-				   $permaLink = get_permalink();
-				   $titleText = get_the_title();
-				   echo '<h1 class="archive-post-title"><a href="'. $permaLink .'">'. $titleText .'</a></h1>';
-			     ?>
-        
-			  <?php
-				   edit_the_link($the_id);
-			     ?>
-		  </header>
-      
+	<?php
+			endwhile;
+		else:
+			echo '<p>Sorry, no posts matched your criteria</p>';
+	  endif;
+	?>
+
+	<section id="project-latest-topics-list">
+		<h1>プロジェクトの最新の投稿</h1>
+		<?php
+			$cat_id = get_post_meta($post->ID, 'cat', true);
+			$args = array('posts_per_page' => 5,
+			              'category' => $cat_id);
+			$posts = get_posts($args);
+			if($posts):
+				foreach($posts as $post):
+					setup_postdata($post);
+					$the_id = get_the_ID();
+		?>
+
+		<article class="archive-post">
+			<header class="archive-post_header">
+				<?php
+					/*
+					 * articles title
+					 */
+					$permaLink = get_permalink();
+					$titleText = get_the_title();
+					echo '<h1 class="archive-post-title"><a href="'. $permaLink .'">'. $titleText .'</a></h1>';
+				?>
+
+				<?php
+					edit_the_link($the_id);
+				?>
+			</header>
+
 		  <footer class="meta-container">
-        
-			  <div class="postmeta-project">
-				  <?php
-					   the_project_list_of_the_post($the_id);
-				     ?>
-			  </div>
-        
-		  </footer>
-      
-		  <div class="entry-body">
-			  <?php the_excerpt() ?>
-		  </div>
-      
-		  <footer class="entry-footer">
-			  <div class="postmeta">
-				  <p class="postmeta-title">投稿者</p>
-				  <address class="postmeta-content">
-					  <?php
-						   $post = get_post($the_id);
-						   $userID = $post->post_author;
-						echo get_avatar($userID, 15);//avatar image
-						echo the_author_posts_link();//auther link
-					  ?>
-				  </address>
-				  <p class="postmeta-title">投稿日時</p>
-				  <div class="postmeta-content">
-					  <?php
-						   the_time_of_the_post($the_id);
-					     ?>
-				  </div>
-			  </div>
-		  </footer>
-	  </article>
-    <?php endforeach;
-          endif; ?>
-  </section>
+				<div class="postmeta">
+					<p class="postmeta-title">投稿者</p>
+					<address class="postmeta-content">
+						<?php
+							$post = get_post($the_id);
+							$userID = $post->post_author;
+							echo get_avatar($userID, 15);//avatar image
+							echo the_author_posts_link();//auther link
+						?>
+					</address>
+					<p class="postmeta-title">投稿日時</p>
+					<div class="postmeta-content">
+						<?php
+							the_time_of_the_post($the_id);
+						?>
+					</div>
+				</div>
+			</footer>
+		</article>
+	<?php
+			endforeach;
+		endif;
+	?>
+	</section>
 </article>
 
-<?php get_footer(); ?>
+<?php
+	get_footer();
+?>
