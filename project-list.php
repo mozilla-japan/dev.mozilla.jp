718b67e56420e76531c28d29ea139715f17e9037
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-05-22 22:18:32 +0900

    project-list.phoのマークアップの修正(#79)

--- project-list.php
+++ project-list.php
@@ -6,8 +6,9 @@ get_header();
 ?>
 
 <article id="content">
+
   <header class="entry-header">
-    <h1>プロジェクト</h1>
+    <h1 class="post-title">プロジェクト</h1>
     <p>
       <a class="button button-white"
          href="<?php echo admin_url('post-new.php?post_type=project'); ?>">
@@ -15,48 +16,63 @@ get_header();
       </a>
     </p>
   </header>
-      <?php
-         $paged = get_query_var('paged');
-         $posts = query_posts(array("post_type" => array("project"),
-                                    "paged" => $paged,
-                                    "posts_par_page" => 5,
-                                    "meta_key " => $_GET['key'],
-                                    "meta_value" => $_GET['value'],
-                                    "orderby" => "date"));
-      ?>
-      <nav class="navigation">
-        <?php navigation_bar(); ?>
-      </nav>
-      <?php
-        if(have_posts()) :
-          foreach ($posts as $post) :
-            setup_postdata($post);
-      ?>
-    <section class="project_summary">
-      <div class="project_icon">
+
+  <?php
+     $paged = get_query_var('paged');
+     $posts = query_posts(array("post_type" => array("project"),
+                                "paged" => $paged,
+                                "posts_par_page" => 5,
+                                "meta_key " => $_GET['key'],
+                                "meta_value" => $_GET['value'],
+                                "orderby" => "date"));
+  ?>
+
+  <nav class="navigation">
+    <?php navigation_bar(); ?>
+  </nav>
+
+  <?php
+    if (have_posts()) :
+      foreach ($posts as $post) :
+        setup_postdata($post);
+  ?>
+
+    <section class="project-section">
+
+      <div class="project-icon">
         <?php
-         if( has_post_thumbnail($post->ID)){
+          if (has_post_thumbnail($post->ID)) {
            the_post_thumbnail();
-         }
-         //サムネイルがない場合はない場合で表示する必要はないのでは。
-         ?>
+          }
+        ?>
       </div>
-      <h1 class="project_title">
-        <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
-          <?php the_title(); ?>
+
+      <h1 class="project-title">
+        <?php
+          $title = get_the_title();
+        ?>
+        <a href="<?php the_permalink() ?>" title="<?php echo $title; ?>">
+          <?php echo $title; ?>
         </a>
       </h1>
-      <p class="project_description">
+ 
+      <p class="project-summary">
         <?php the_excerpt(); ?>
       </p>
+
     </section>
-    <?php
-       endforeach;
-       endif;
-    ?>
-    <nav class="navigation">
-      <?php navigation_bar(); ?>
-    </nav>
+
+  <?php
+      endforeach;
+    endif;
+  ?>
+
+  <nav class="navigation">
+    <?php navigation_bar(); ?>
+  </nav>
+
 </article>
 
-<?php get_footer(); ?>
+<?php
+  get_footer();
+?>
