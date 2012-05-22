a3dd56257af37c17f824410b490f86828b69d4c5
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-05-22 23:21:47 +0900

    faviconをico形式に(#122)

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
   <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <link rel="alternate" type="application/rss+xml" title="modestの最新の投稿" href="<?php bloginfo('rss2_url'); ?>" />
-  <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.png" type="image/png"/>
+  <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
 
   <!--[if lt IE 9]>
     <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
