498c10655e2a431012cef6007a985bf7bd8b2fd2
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-05-28 18:18:57 +0900

    viewportの指定

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
-  <meta name="viewport" content="width=device-width" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <title>
     <?php wp_title('|', true, 'right'); ?>
