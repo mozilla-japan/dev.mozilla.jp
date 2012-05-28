150298b5a57a556ddc87c0ff5dfd365ddda79626
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-05-28 16:49:29 +0900

    ページごとの検索結果表示数を変更(#153)

--- search.php
+++ search.php
@@ -8,7 +8,10 @@ get_header(); ?>
 
 
 <article id="content" role="main">
-  <?php if (have_posts()) : ?>
+  <?php
+    query_posts($query_string .'&posts_per_page=20');
+    if (have_posts()) :
+  ?>
   <h1 class="post-title"><?php the_search_query(); ?> の検索結果</h1>
   <nav class="navigation">
     <?php navigation_bar(); ?>
