de95c34266bf79193942bf135b37ae1ac12f25e8
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-06-01 19:28:32 +0900

    clean up

--- archive.php
+++ archive.php
@@ -32,14 +32,16 @@ get_header();
 				//post_icon(get_the_ID(),array(70,70));
 			?>
 
-			<?php
-				/*
-				* article's title
-				*/
-				$permaLink = get_permalink();
-				$titleText = get_the_title();
-				echo '<h1 class="archive-post-title"><a href="'. $permaLink .'">'. $titleText .'</a></h1>';
-			?>
+			<h1 class="archive-post-title">
+				<?php
+					/*
+					* article's title
+					*/
+					$permaLink = get_permalink();
+					$titleText = get_the_title();
+					echo '<a href="'. $permaLink .'">'. $titleText .'</a>';
+				?>
+			</h1>
 
 			<?php
 				edit_the_link($the_id);
