651cb0f5c863857f903ad53644f53588ad4102a4
saneyuki_s <saneyuki.s.snyk@gmail.com>
2012-06-12 15:29:43 +0900

    イベント地図のキャプションを作成する

--- functions.php
+++ functions.php
@@ -1182,7 +1182,7 @@ function map_image_of_the_event ($id, $size='380') {
   echo <<< DOC
 <a href="$url">
   <img src="$img_src" alt="$alt" /><br />
-  <span>Google Map で表示する</span>
+  <span class="map-caption">Google Map で大きい地図を表示する</span>
 </a>
 DOC;
 }
