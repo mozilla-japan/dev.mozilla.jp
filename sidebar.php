1363e1c0c58fc4c1219ff747d2c33779ca1b6f39
Keiichi <ghost@ethertank.jp>
2013-03-20 22:32:30 +0900

    ボタン部分を見出し要素からリストアイテムに変更

--- sidebar.php
+++ sidebar.php
@@ -7,22 +7,17 @@
 
 <nav id="sidebar">
 
-  <div id="sidebar-button-group">
-
+  <ul id="sidebar-button-group">
     <?php if(is_user_logged_in() == false) : ?>
-      <h3><a href="<?php echo get_join_modest_url(); ?>" class="button-blue join">ログインする/参加する</a></h3>
+    <li><a href="<?php echo get_join_modest_url(); ?>" class="button-blue join">ログインする/参加する</a></li>
     <?php endif; ?>
+    <li><a href="<?php echo get_add_post_url(); ?>" class="button-blue join">トピックを投稿する</a></li>
 
-    <h3><a href="<?php echo get_add_post_url(); ?>" class="button-blue join">トピックを投稿する</a></h3>
-
-    <h3><a href="<?php echo get_add_event_url(); ?>" class="button-blue join">イベントを追加する</a></h3>
-
+    <li><a href="<?php echo get_add_event_url(); ?>" class="button-blue join">イベントを追加する</a></li>
     <?php if( is_user_logged_in() ) : ?>
-      <h3><a href="<?php echo get_add_project_url(); ?>" class="button-blue join">プロジェクトを作成</a></h3>
-
+    <li><a href="<?php echo get_add_project_url(); ?>" class="button-blue join">プロジェクトを作成</a></li>
     <?php endif; ?>
-
-  </div>
+  </ul>
 
   <h3 id="sidebar-about"><a href="<?php echo get_about_url(); ?>">このサイトについて</a></h3>
 
