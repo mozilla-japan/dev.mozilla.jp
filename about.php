60273e572107d9ff24084aa5d0ab71ae1f8a0754
Keiichi <ghost@ethertank.jp>
2013-04-08 05:37:03 +0900

    fmt

--- about.php
+++ about.php
@@ -1,1 +1,12 @@
-<?php/* * Template Name: about */get_header(); ?><artcle id="content">  <h1><?php the_title(); ?></h1>  <p><?php the_content() ?></p></artcle><?php get_footer(); ?>
+<?php
+/*
+ * Template Name: about
+ */
+get_header(); ?>
+
+<artcle id="content">
+  <h1><?php the_title(); ?></h1>
+  <p><?php the_content() ?></p>
+</artcle>
+
+<?php get_footer(); ?>
